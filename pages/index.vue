b9e31dc86b99b3629531bf58db5df1bb2ffe74d3
Nafis Handoko <naffisioner@gmail.com>
2023-03-08 08:26:26 +0700

    add skills section

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <header class="bg-center bg-cover text-white bg-[url('https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')]">
+  <header
+    class="bg-center bg-cover text-white bg-[url('https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')]">
     <div class='bg-black/50 min-h-screen'>
       <nav class='container mx-auto flex flex-col md:flex-row items-center md:justify-center w-full px-28 py-10'>
         <ul class='flex flex-row gap-x-7 items-center text-light'>
@@ -15,9 +16,41 @@
       </div>
     </div>
   </header>
-  <section class="bg-[#141414] py-10">
+  <section class="bg-[#141414] py-20">
     <div class="container mx-auto flex flex-col items-center">
-      <h2 class="font-bold text-lg text-white">What i do</h2>
+      <h2 class="font-bold text-2xl text-white mb-7">What i do</h2>
+      <div class="flex flex-row items-center gap-7 text-white px-32">
+        <div v-for="skill in skills" :key="skill.id">
+          <div class="bg-[#252525] rounded border border-[#252525] hover:border-white p-5 pt-10 space-y-2">
+            <span class="font-bold text-lg">{{ skill.id }}</span>
+            <h3 class="font-bold text-lg">{{ skill.title }}</h3>
+            <p class="text-white/[.5]">{{ skill.desc }}</p>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
+  <section>
+    
+  </section>
 </template>
+
+<script setup>
+const skills = ref([
+  {
+    id: 1,
+    title: "Web Development",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut eum, voluptatibus debitis natus repellendus fugiat!"
+  },
+  {
+    id: 2,
+    title: "UI/UX Design",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut eum, voluptatibus debitis natus repellendus fugiat!"
+  },
+  {
+    id: 3,
+    title: "Mobile Development",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut eum, voluptatibus debitis natus repellendus fugiat!"
+  }
+])
+</script>
