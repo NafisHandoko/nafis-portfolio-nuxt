f9ac72faf4419f0e9f3169da549fbd85832b6314
Nafis Handoko <naffisioner@gmail.com>
2023-03-08 21:02:15 +0700

    add projects section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,9 +9,7 @@ export default defineNuxtConfig({
       link: [
         // <link rel="stylesheet" href="https://myawesome-lib.css">
         { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css' },
-        { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css' },
-        { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css' },
-        { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css' }
+        { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css' },
       ],
     }
   }
