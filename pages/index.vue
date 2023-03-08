e5c1ee71d3d1ade3ee904310adc5edc7cae0a99c
Nafis Handoko <naffisioner@gmail.com>
2023-03-08 07:38:10 +0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="bg-center bg-cover text-white bg-[url('https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')]">
+    <div class='bg-black/50 min-h-screen'>
+      <nav class='container mx-auto flex flex-col md:flex-row items-center md:justify-center w-full px-28 py-10'>
+        <ul class='flex flex-row gap-x-7 items-center text-light'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Works</a></li>
+        </ul>
+      </nav>
+      <div
+        class='min-h-[75vh] container mx-auto w-full flex flex-col items-center justify-center gap-y-5 text-center px-10 md:px-0'>
+        <h1 class='font-bold text-3xl'>WEB DEVELOPER</h1>
+      </div>
+    </div>
+  </header>
+  <section class="bg-[#141414] py-10">
+    <div class="container mx-auto flex flex-col items-center">
+      <h2 class="font-bold text-lg text-white">What i do</h2>
+    </div>
+  </section>
+</template>
