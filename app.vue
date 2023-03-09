44efe99e25b50c7ebc7b92c052fe97a105bf07fb
Nafis Handoko <naffisioner@gmail.com>
2023-03-09 17:02:28 +0700

    add animation

--- app.vue
+++ app.vue
@@ -14,11 +14,24 @@ useHead({
     },
   ],
 });
+const slide = ref(false)
+const welcome = () => {
+  setTimeout(() => {
+    slide.value = true
+  }, 3000)
+}
+welcome()
 </script>
 
 <template>
   <NuxtLayout>
     <Layout>
+      <div class="bg-black-primary w-full h-screen fixed z-10 bottom-0 left-0 transition-all"
+        :class="{ 'bottom-[100%]': slide }">
+        <div class="container mx-auto flex items-center justify-center h-full">
+          <h1 class="font-bold text-3xl text-white tracking-[20px] animate-bounce">HELLO!</h1>
+        </div>
+      </div>
       <NuxtPage />
     </Layout>
   </NuxtLayout>
