9eef3a7e7cc03e2b97dc372bdc93aba6e9df07bd
Nafis Handoko <naffisioner@gmail.com>
2023-03-09 14:59:25 +0700

    add a glowing effect on navigation link

--- pages/index.vue
+++ pages/index.vue
@@ -16,19 +16,23 @@
   </div>
   <header
     class="bg-center bg-fixed bg-cover text-white bg-[url('https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')]">
-    <div class='bg-black/50 min-h-screen'>
+    <div class='bg-black/70 min-h-screen'>
       <nav class='container mx-auto flex flex-col md:flex-row items-center md:justify-center w-full px-28 py-10'>
         <ul class='flex flex-row gap-x-7 items-center text-light'>
-          <li class="indexLi"><a href="#">Home</a></li>
-          <li class="indexLi"><a href="#">About</a></li>
-          <li class="indexLi"><a href="#">Services</a></li>
-          <li class="indexLi"><a href="#">Skills</a></li>
-          <li class="indexLi"><a href="#">Works</a></li>
+          <li class="border border-transparent hover:border-b-white transition-all"><a class="glow transition-all" href="#">Home</a></li>
+          <li class="border border-transparent hover:border-b-white transition-all"><a class="glow transition-all" href="#">About</a>
+          </li>
+          <li class="border border-transparent hover:border-b-white transition-all"><a class="glow transition-all" href="#">Services</a>
+          </li>
+          <li class="border border-transparent hover:border-b-white transition-all"><a class="glow transition-all" href="#">Skills</a>
+          </li>
+          <li class="border border-transparent hover:border-b-white transition-all"><a class="glow transition-all" href="#">Works</a>
+          </li>
         </ul>
       </nav>
       <div
         class='min-h-[75vh] container mx-auto w-full flex flex-col items-center justify-center gap-y-5 text-center px-10 md:px-0'>
-        <h1 class='font-bold text-3xl'>WEB DEVELOPER</h1>
+        <h1 class='font-bold text-3xl'>NAFIS HANDOKO</h1>
       </div>
     </div>
   </header>
@@ -45,9 +49,10 @@
       <h2 class="font-bold text-2xl mb-7">What i do</h2>
       <div class="flex flex-row items-stretch gap-7">
         <div v-for="(service, index) in services" :key="index">
-          <div class="bg-black-secondary rounded border border-black-secondary hover:border-white p-5 h-full flex flex-col items-start">
+          <div
+            class="bg-black-secondary rounded border border-black-secondary hover:border-white p-5 h-full flex flex-col items-start">
             <i class="self-center text-3xl py-5" :class="service.icon"></i>
-            <span class="font-bold text-lg">{{ index+1 }}</span>
+            <span class="font-bold text-lg">{{ index + 1 }}</span>
             <h3 class="font-bold text-lg">{{ service.title }}</h3>
             <p class="text-white/[.5]">{{ service.desc }}</p>
           </div>
@@ -71,11 +76,14 @@
       <h2 class="font-bold text-2xl mb-7">My projects</h2>
       <div class="grid grid-cols-2 gap-7 text-black">
         <div class="bg-white rounded-xl p-2 hover:scale-105 transition-all" v-for="(project, index) in projects">
-          <div class="bg-center bg-cover w-full h-[150px] shadow-xl rounded-lg bg-[url('https://images.unsplash.com/photo-1551650975-87deedd944c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80')]"></div>
+          <div
+            class="bg-center bg-cover w-full h-[150px] shadow-xl rounded-lg bg-[url('https://images.unsplash.com/photo-1551650975-87deedd944c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80')]">
+          </div>
           <div class="flex flex-row items-center p-4 gap-3">
             <div class="flex flex-col gap-1">
               <h3 class="font-bold">Social Media App</h3>
-              <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat qui officia corporis tempora nihil voluptate.</p>
+              <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat qui officia corporis
+                tempora nihil voluptate.</p>
             </div>
             <div class="text-lg flex flex-col items-center gap-1">
               <a class="" href="https://github.com/nafishandoko"><i class="bi bi-github"></i></a>
@@ -178,7 +186,7 @@ const projects = ref([
 </script>
 
 <style scoped>
-.indexLi:after {
+.link:after {
   width: 0;
   height: 1px;
   background-color: white;
@@ -189,7 +197,17 @@ const projects = ref([
   transition: width 0.2s ease-in-out;
 }
 
-.indexLi:hover:after {
+.link:hover:after {
   width: 100%;
+}
+
+.glow:hover {
+  /* text-shadow: 0 0 50px white; */
+  text-shadow:
+    0 0 30px white,
+    0 0 30px white,
+    0 0 30px white,
+    0 0 30px white;
+  filter: dropshadow(color=white, offx=2, offy=2);
 }
 </style>
