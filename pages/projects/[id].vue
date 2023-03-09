4029fe7968c303b0d0d9d670407ff77859ea3dd9
Nafis Handoko <naffisioner@gmail.com>
2023-03-09 10:55:33 +0700

    add image, techstack and description in project detail page

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,6 +1,28 @@
 <template>
-    <section class="bg-black-primary text-white">
-        <div class="container mx-auto flex flex-col items-center"></div>
-        <h1 class="font-bold text-2xl mb-7 text-center">Project Detail</h1>
+    <section class="bg-black-primary text-white py-12">
+        <div class="container mx-auto flex flex-col items-center">
+            <h1 class="font-bold text-2xl mb-10 text-center">Project Detail</h1>
+            <div class="flex flex-row items-start gap-10">
+                <!-- <div class="bg-center bg-cover w-full aspect-square shadow-xl rounded-lg bg-[url('https://images.unsplash.com/photo-1551650975-87deedd944c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80')]"></div> -->
+                <img class="w-1/2 rounded-lg"
+                    src="https://images.unsplash.com/photo-1551650975-87deedd944c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+                    alt="">
+                <div class="w-1/2 flex flex-col items-start gap-3">
+                    <h2 class="font-bold text-xl">Social Media App</h2>
+                    <p>Tech Stack : React, Tailwind, Golang, PostgreSQL</p>
+                </div>
+            </div>
+        </div>
     </section>
-</template>
+    <section class="bg-black-primary text-white">
+        <div class="container mx-auto flex flex-col gap-3">
+            <h2 class="font-bold text-xl">Description</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vitae quaerat alias! Esse ea commodi odit
+                ad, dolor impedit magni cumque aut maxime amet nobis, consectetur doloribus libero recusandae sint eius
+                architecto alias officia facilis nihil! Possimus doloremque amet quam animi quis saepe nisi reprehenderit
+            ullam sunt officia, iure nam id temporibus, quos libero beatae harum adipisci nobis maiores facilis ut
+            distinctio necessitatibus consectetur. Aperiam mollitia iusto praesentium alias culpa magni saepe, quibusdam
+            provident, dolores enim fugit suscipit ea voluptates non vitae! Sint, adipisci fuga ducimus totam est
+            dolorem accusamus consequuntur earum quae asperiores ea! Ab officia id quasi quaerat.</p>
+    </div>
+</section></template>
