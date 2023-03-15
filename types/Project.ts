ec81d1159ddf5eef55c98bd8294dfa9c93793752
Nafis Handoko <naffisioner@gmail.com>
2023-03-15 12:49:03 +0700

    add Project interface

---
+++ types/Project.ts
@@ -0,0 +1,10 @@
+interface Project {
+    title: string,
+    shortDesc: string,
+    longDesc: string,
+    repo: string,
+    demo: string,
+    img: string,
+    techStack: string,
+    tag: string[]
+}
